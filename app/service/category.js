326c2e8cd9bfb7fe61923b7adbe937edecbd6a31
Ted.Zhou <zhoutiedao@gmail.com>
2016-11-28 08:00:28 +0000

    TASK #549 修改品类关联修改下属产品

--- app/service/category.js
+++ app/service/category.js
@@ -1,4 +1,6 @@
 const daoCate = require(appPath + '/dao/category.js')
+const daoProduct = require(appPath + '/dao/product.js')
+const daoProductDiscard = require(appPath + '/dao/product-discard.js')
 // const daoCateImg = require(appPath + '/dao/category-img.js')
 
 let srvCate = {}
@@ -73,45 +75,6 @@ function catesToTreeNodes(cates, opts){
 }
 
 
-function checkCode(cate, oCode){
-    let p = new Promise((resolve, reject) => {
-        let err = null
-        if (!cate || !cate.code){
-            err = new Error('编码不能为空！'), err.level = 0
-            return reject(err)
-        }
-        let cond = {$not: {_id: cate._id}}
-        let rPcode = new RegExp('^'+oCode)
-        let rCode = new RegExp('^'+cate.code)
-        cond.$where = function(){
-            if (err && err.level <= 1) return false
-            if (cate._id && (this._id == cate._id || this.pcode.match(rPcode))){//跳过本身及下级
-                return false
-            }
-            if (this.code == cate.code){
-                err = new Error('编码已存在！'), err.level = 0
-                return true
-            }
-            if (err && err.level <= 2) return false
-            if (this.code.match(rCode)){
-                err = new Error('编码不能是已有编码的前缀！'), err.level = 2
-                return true
-            }
-            if (err && err.level <= 3) return false
-            if (cate.pcode.search(this.pcode) < 0 && cate.code.search(this.code) === 0){
-                err = new Error('编码的前缀不能是已有编码！'), err.level = 3
-                return true
-            }
-            return false
-        }
-        let p1 = daoCate.count(cond)
-        return p1.then(cnt => {
-            if (cnt) return reject(err)
-            return resolve(cnt)
-        })
-    })
-    return p
-}
 srvCate.save = function(cate){
     let nCode, oCode = cate && cate.code
     if (cate){//合成code
@@ -125,35 +88,80 @@ srvCate.save = function(cate){
                 if (!rst || !oCode){
                     return resolve(rst)
                 }
-                //查找出所有下级品类，更改其编码
-                let likey = new RegExp('^'+oCode)
-                let p = daoCate.find({pcode: likey}).then(scates => {
-                    for (let scate of scates){
-                        scate.pcode = scate.pcode && scate.pcode.replace(oCode, nCode)
-                        scate.code = scate.pcode + scate.scode
-                    }
-                    return daoCate.upsert(scates).then(rst => {
-                        resolve(rst)
-                    })
+                let p = updateSubs(oCode, nCode).then(() => {
+                    resolve(rst)
                 })
                 return p
             })
         }).catch(err => reject(err))
     })
     return p
-    // return daoCate.save(cate).then(rst => {
-    //     if (!rst || !oCode) return rst
-    //     //查找出所有下级品类，更改其编码
-    //     let likey = new RegExp('^'+oCode)
-    //     let p = daoCate.find({pcode: likey}).then(scates => {
-    //         for (let scate of scates){
-    //             scate.pcode = scate.pcode && scate.pcode.replace(oCode, nCode)
-    //             scate.code = scate.pcode + scate.scode
-    //         }
-    //         return daoCate.upsert(scates)
-    //     })
-    //     return p
-    // })
+    function checkCode(cate, oCode){
+        let p = new Promise((resolve, reject) => {
+            let err = null
+            if (!cate || !cate.code){
+                err = new Error('编码不能为空！'), err.level = 0
+                return reject(err)
+            }
+            let cond = {$not: {_id: cate._id}}
+            let rPcode = new RegExp('^'+oCode)
+            let rCode = new RegExp('^'+cate.code)
+            cond.$where = function(){
+                if (err && err.level <= 1) return false
+                if (cate._id && (this._id == cate._id || this.pcode.match(rPcode))){//跳过本身及下级
+                    return false
+                }
+                if (this.code == cate.code){
+                    err = new Error('编码已存在！'), err.level = 0
+                    return true
+                }
+                if (err && err.level <= 2) return false
+                if (this.code.match(rCode)){
+                    err = new Error('编码不能是已有编码的前缀！'), err.level = 2
+                    return true
+                }
+                if (err && err.level <= 3) return false
+                if (cate.pcode.search(this.pcode) < 0 && cate.code.search(this.code) === 0){
+                    err = new Error('编码的前缀不能是已有编码！'), err.level = 3
+                    return true
+                }
+                return false
+            }
+            let p1 = daoCate.count(cond)
+            return p1.then(cnt => {
+                if (cnt) return reject(err)
+                return resolve(cnt)
+            })
+        })
+        return p
+    }
+    function updateSubs(oCode, nCode){
+        let likey = new RegExp('^'+oCode)
+        //查找出所有下级品类，更改其编码
+        let p1 = daoCate.find({pcode: likey}).then(scates => {
+            for (let scate of scates){
+                scate.pcode = scate.pcode && scate.pcode.replace(oCode, nCode)
+                scate.code = scate.pcode + scate.scode
+            }
+            return daoCate.upsert(scates)
+        })
+        //查找出所有下级品类，更改其编码
+        let p2 = daoProduct.find({'category.code': likey}, {category:1}).then(pds => {
+            for (let pd of pds){
+                if (!pd.category || !pd.category.code) continue
+                pd.category.code = pd.category.code.replace(oCode, nCode)
+            }
+            daoProduct.upsert(pds)
+        })
+        let p3 = daoProductDiscard.find({'category.code': likey}, {category:1}).then(pds => {
+            for (let pd of pds){
+                if (!pd.category || !pd.category.code) continue
+                pd.category.code = pd.category.code.replace(oCode, nCode)
+            }
+            daoProductDiscard.upsert(pds)
+        })
+        return p1 // let p2/3 be!
+    }
 }
 
 srvCate.removeById = function(id){
